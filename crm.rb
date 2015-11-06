60d55e2a8a69a586d0aec59b27f9d0ae9cf3bdec
Lauren <lemherdman@hotmail.com>
2015-11-06 15:25:13 -0500

    Datamapper stores data

--- crm.rb
+++ crm.rb
@@ -1,15 +1,28 @@
-require_relative 'contacts'
 require 'sinatra'
+require 'data_mapper'
 
-# Temporary fake data so that we always find contact with id 1000.
+DataMapper.setup(:default, "sqlite3:database.sqlite3")
+class Contact
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :first_name, String
+  property :last_name, String
+  property :email, String
+  property :note, String
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
 
 get '/' do
 	@crm_app_name = "My CRM"
 	erb :index
 end
 
-get '/contacts' do
-	erb :contacts
+get "/contacts" do
+  @contacts = Contact.all
+  erb :contacts
 end
 
 get '/contacts/new' do
@@ -24,23 +37,27 @@ get '/contacts/modify' do
 	erb :modify_contact
 end
 
-post '/contacts' do
-  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+post "/contacts" do
+  contact = Contact.create(
+    :first_name => params[:first_name],
+    :last_name => params[:last_name],
+    :email => params[:email],
+    :note => params[:note]
+  )
   redirect to('/contacts')
-
 end
 
 get "/contacts/:id" do
-  @contact = Contact.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
-  	erb :show_contact
+    erb :show_contact
   else
-  	raise Sinatra::NotFound
+    raise Sinatra::NotFound
   end
 end
 
 get "/contacts/:id/edit" do
-  @contact = Contact.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
     erb :edit_contact
   else
@@ -49,12 +66,14 @@ get "/contacts/:id/edit" do
 end
 
 put "/contacts/:id" do
-  @contact = Contact.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
-    @contact.first_name = params[:first_name]
-    @contact.last_name = params[:last_name]
-    @contact.email = params[:email]
-    @contact.notes = params[:note]
+  	Contact.update(
+	    :first_name => params[:first_name],
+	    :last_name => params[:last_name],
+	    :email => params[:email],
+	    :note => params[:note]
+    )
 
     redirect to("/contacts")
   else
@@ -63,9 +82,9 @@ put "/contacts/:id" do
 end
 
 delete "/contacts/:id" do
-  @contact = Contact.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
-    @contact.remove
+    @contact.destroy
     redirect to("/contacts")
   else
     raise Sinatra::NotFound
